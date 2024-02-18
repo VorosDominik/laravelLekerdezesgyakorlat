11c48f71064ba930aa96de39ff8000d690fe3bfa
VorosDominik <medoxidhun@gmail.com>
2024-02-18 20:05:01 +0100

    solo

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\AirlineController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('airlines', [AirlineController::class, 'index']);
+Route::get('airlines/{id}', [AirlineController::class, 'show']);
+Route::post('airlines', [AirlineController::class, 'store']);
+Route::put('airlines/{id}', [AirlineController::class, 'update']);
